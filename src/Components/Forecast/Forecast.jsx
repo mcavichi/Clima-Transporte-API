b42abcfeeac87c67522832ebe595c94f3fc0368d
Marcos Cavichioli <mcavichi@gmail.com>
2023-10-13 20:48:27 -0300

    Modificando Componente Forecast

--- src/Components/Forecast/Forecast.jsx
+++ src/Components/Forecast/Forecast.jsx
@@ -9,26 +9,24 @@ const Forecast = (props) => {
     const chartTheme = {
         palette: {
             primary: {
-                main: '#FF8000', // Change the color of the chart area here
+                main: 'FF8000', 
             },
         },
     };
     return (
         <div className="ForecastContainer">
-            <div>
-                <h4 className='ForecastText'>Forecast</h4>
-            </div>
-            <div className='Flex' style={{ width: '100%', height: '15px' }}>
+            <div className='Flex' style={{ width: '100%', height: '70px' }}>
                 <LineChart 
                     xAxis={[{ data: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23] }]}
                     series={[
                         {
+                        label: 'Temperatura por hora',
                         data: props.Temp,
                         area: true,
                         },
                     ]}
                     width={800}
-                    height={250}
+                    height={220}
                     xAxisLabel="Hora"
                     yAxisLabel="Temperatura"
                     theme={chartTheme}
